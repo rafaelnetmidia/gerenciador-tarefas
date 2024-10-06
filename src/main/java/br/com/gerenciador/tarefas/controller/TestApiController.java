ca1710ddbf4f7863a53ec110f72db3ca3c2f966a
rafaelnetmidia <32550511+rafaelnetmidia@users.noreply.github.com>
2024-10-06 00:17:00 -0300

    criado rota de teste com parametros.

--- src/main/java/br/com/gerenciador/tarefas/controller/TestApiController.java
+++ src/main/java/br/com/gerenciador/tarefas/controller/TestApiController.java
@@ -1,14 +1,20 @@
 package br.com.gerenciador.tarefas.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestApiController {
 
     @GetMapping("/test-api")
-    private String test() {
+    public String test() {
         return "Success";
+    }
+
+    @GetMapping("/test-api-hello-world")
+    public String testHelloWorld(@RequestParam(name = "name") String name) {
+        return "Olá "+name+", seja bem vindo!";
     }
 
 }
