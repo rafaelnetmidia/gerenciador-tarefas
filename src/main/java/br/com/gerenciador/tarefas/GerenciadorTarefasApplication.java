9ab05f640a150cd0cb82febe30c9aa1698345f25
rafaelnetmidia <32550511+rafaelnetmidia@users.noreply.github.com>
2024-10-05 23:58:09 -0300

    primeiro commit do projeto.

---
+++ src/main/java/br/com/gerenciador/tarefas/GerenciadorTarefasApplication.java
@@ -0,0 +1,13 @@
+package br.com.gerenciador.tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorTarefasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciadorTarefasApplication.class, args);
+    }
+
+}
